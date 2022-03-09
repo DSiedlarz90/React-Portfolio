0234832fb43aed11d564e10c6b83112c66766309
DSiedlarz90 <dsiedlarz90@gmail.com>
2022-03-09 12:36:44 -0500

    added nav bar to page

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+function About() {
+    return(
+        <section class='about'>
+            <div class='about-me'>
+                <h2>About Me</h2>
+                <p>Testing Testing Testicles</p>
+            </div>
+        </section>
+    )
+}
+
+export default About;
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+function Nav() {
+    return(
+        <header className= "header">
+            <h1 className="heading">David Siedlarz</h1>
+                <nav>
+                    <ul className="nav">
+                        <li>
+                            <a href="#about">About Me</a>
+                        </li>
+                        <li>
+                            <a href="#projects">Projects</a>
+                        </li>
+                        <li>
+                            <a href="#contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+        </header>
+    )
+}
+
+export default Nav; 
