f987bb274cc6ef59eedd4eee2cffaf1e1c69ec47
DSiedlarz90 <dsiedlarz90@gmail.com>
2022-03-15 16:37:57 -0400

    added resume page with styling and download link to current resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,24 @@
 function Resume() {
     return(
-        <section className='Resume'>
-            <h2>Download my resume here!</h2>
-            <p>Testing testing testicles</p>
-        </section>
+        <div className="resume-container">
+            <h1 className="resume-head">Resume</h1>
+            <div className="resume-list">
+                <ul className="list-items"> <u className="underline">Front end</u>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                    <li>React</li>
+                </ul>
+                <ul className="list-items"> <u className="underline">Back end</u>
+                    <li>Node</li>
+                    <li>Express</li>
+                    <li>SQL</li>
+                    <li>PWAs</li>
+                    <li>NoSQL</li>
+                </ul>
+            </div>
+            <a className="resume-link" href="resume/Resume.DOCX" download="David Siedlarz Resume">Click to download my resume</a>
+        </div>
     )
 }
 
