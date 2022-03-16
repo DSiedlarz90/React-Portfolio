b267d12f4515bd00e7ed40ecab5c99ab7b4ea919
DSiedlarz90 <dsiedlarz90@gmail.com>
2022-03-16 18:43:36 -0400

    added projects with stlying. adjusted some styling in the header

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,10 +1,35 @@
-function Projects() {
-    return(
-        <section className='projects'>
-            <h2>Current Projects and Applications</h2>
-            <p>Testing testing testicles</p>
-        </section>
-    )
-}
+import cryptoquotes from '../../images/cryptoquotes.jpg'
+import personalityTest from '../../images/personalityTest.jpg'
+const Portfolio = () => {
 
-export default Projects;
+     return (
+        <section className="project-container">
+        <div>
+            <h3 className="project-head">Current Work</h3>
+        </div>
+        <div className="work">
+        <article>
+            <div>
+                <p className="link-head">CryptoQuotes</p>
+                <p className="link-desc">HTML/CSS/JS/API's/TailwindCSS</p>
+            </div>
+            <a href="https://krissmith7.github.io/cryptoquotes/">
+            <img className="projectimg" src={cryptoquotes} alt="homepage of cryptoquotes web app"/>
+            </a>
+        </article>
+        <article>
+            <div>
+                <p className="link-head">Personality Test</p>
+                <p className="link-desc">Node/Javascript/Express/Mysql2</p>
+            </div>
+            <a href="https://hidden-plains-33333.herokuapp.com/">
+            <img className="projectimg" src={personalityTest} alt="Screenshot of a test question"/>
+            </a>
+        </article>
+        </div>
+        </section>
+        );
+    };
+    
+
+export default Portfolio;
